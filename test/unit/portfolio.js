a73c60549ec92cf6d17b76c9be4c12262d2835dc
Mikey Badr <mfbadr@gmail.com>
2014-07-21 13:09:59 -0500

    portfolio constructor

--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -8,6 +8,7 @@ describe('portfolio', function(){
   describe('constructor', function(){
     it('should create a new portfolio object', function(){
       var test = new Portfolio('name');
+      expect(test).to.be.instanceof(Portfolio);
       expect(test.name).to.equal('name');
       expect(test.stocks.length).to.equal(0);
     });
