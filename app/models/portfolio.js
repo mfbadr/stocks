4b8a4b13b8c7367d974fe68168f9dc8117ce9ba7
Mikey Badr <mfbadr@gmail.com>
2014-07-21 13:02:41 -0500

    added portfolio constructor

---
+++ app/models/portfolio.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function Portfolio(name){
+  this.name = name;
+  this.stocks = [];
+}
+module.exports = Portfolio;
---
+++ test/unit/portfolio.js
@@ -0,0 +1,15 @@
+/* global describe, it */
+'use strict';
+
+var expect = require('chai').expect;
+var Portfolio = require('../../app/models/portfolio');
+
+describe('portfolio', function(){
+  describe('constructor', function(){
+    it('should create a new portfolio object', function(){
+      var test = new Portfolio('name');
+      expect(test.name).to.equal('name');
+      expect(test.stocks.length).to.equal(0);
+    });
+  });
+});
