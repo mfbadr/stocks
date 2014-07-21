09dcf2297f90e877587eee62b9f5fc9fc0ba4e4c
Mikey Badr <mfbadr@gmail.com>
2014-07-21 14:02:21 -0500

    added portfolio

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,7 +1,20 @@
 'use strict';
+var Stock = require('./stock');
 
 function Portfolio(name){
   this.name = name;
   this.stocks = [];
 }
+
+Portfolio.prototype.add = function(symbol, count){
+  for( var i = 0; i < this.stocks.length; i++){
+    if(symbol.toUpperCase() === this.stocks[i].symbol){
+      this.stocks[i].count += count;
+      return;
+    }
+  }  
+  var newStock = new Stock(symbol, count);
+  this.stocks.push(newStock);
+};
+
 module.exports = Portfolio;
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -3,6 +3,7 @@
 
 var expect = require('chai').expect;
 var Portfolio = require('../../app/models/portfolio');
+var Stock = require('../../app/models/stock');
 
 describe('portfolio', function(){
   describe('constructor', function(){
@@ -11,6 +12,20 @@ describe('portfolio', function(){
       expect(test).to.be.instanceof(Portfolio);
       expect(test.name).to.equal('name');
       expect(test.stocks.length).to.equal(0);
+    });
+  });
+  describe('#add', function(){
+    it('should add a stock to the portfolio & check for dupes', function(){
+    var tech = new Portfolio('Tech Stocks');
+    tech.add('aapl', 25);
+    tech.add('amzn', 35);
+    tech.add('aapl', 50);
+    
+    expect(tech.stocks[0]).to.be.instanceof(Stock);
+    expect(tech.stocks).to.have.length(2);
+    expect(tech.stocks[0].count).to.equal(75);
+    expect(tech.stocks[1].count).to.equal(35);
+
     });
   });
 });
